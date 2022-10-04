5aacd71277c692ca4f36e5a45ff2d583bc714695
Ju Minji <alswl0w0@gmail.com>
2022-10-04 14:34:08 +0900

    setting

---
+++ src/main/java/com/ll/exam/sb_jwt_exam/SbJwtExamApplication.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sb_jwt_exam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbJwtExamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbJwtExamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ll/exam/sb_jwt_exam/SbJwtExamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.exam.sb_jwt_exam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbJwtExamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
