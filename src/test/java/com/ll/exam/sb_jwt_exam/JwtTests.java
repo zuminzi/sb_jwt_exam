f391b8be8e4954f33419e301cec05033e17f5da7
Ju Minji <alswl0w0@gmail.com>
2022-10-05 23:58:42 +0900

    [CHORE] secretKey 원문으로 HMAC 암호화 방식에 맞는 SecretKey 객체 만들기

--- src/test/java/com/ll/exam/sb_jwt_exam/JwtTests.java
+++ src/test/java/com/ll/exam/sb_jwt_exam/JwtTests.java
@@ -1,20 +1,30 @@
 package com.ll.exam.sb_jwt_exam;
 
+import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.crypto.SecretKey;
+import java.util.Base64;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtTests {
     @Value("${custom.jwt.secretKey}") // secretKey value annotation
-    private String secretKey;
+    private String secretKeyPlain;
 
     @Test
     @DisplayName("secretKey 키가 존재해야한다.")
-    void t1() {
+    void t1() {assertThat(secretKeyPlain).isNotNull();}
+
+    @Test
+    @DisplayName("sercretKey 원문으로 hmac 암호화 알고리즘에 맞는 SecretKey 객체를 만들 수 있다.")
+    void t2() {
+        String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
+        SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
         assertThat(secretKey).isNotNull();
     }
 }
