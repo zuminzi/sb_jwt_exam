6539a77935bea516204904eb9d1beedde5652911
Ju Minji <alswl0w0@gmail.com>
2022-10-06 18:04:10 +0900

    [CHORE] Provider 컴포넌트에 secretKey 정보 모아서 저장
    *SecretKey 객체를 jwtProvider 객체를 통해서 얻을 수 있음

--- src/test/java/com/ll/exam/sb_jwt_exam/JwtTests.java
+++ src/test/java/com/ll/exam/sb_jwt_exam/JwtTests.java
@@ -1,8 +1,10 @@
 package com.ll.exam.sb_jwt_exam;
 
+import com.ll.exam.sb_jwt_exam.app.jwt.JwtProvider;
 import io.jsonwebtoken.security.Keys;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -13,6 +15,8 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class JwtTests {
+    @Autowired
+    private JwtProvider jwtProvider;
     @Value("${custom.jwt.secretKey}") // secretKey value annotation
     private String secretKeyPlain;
 
@@ -20,11 +24,22 @@ class JwtTests {
     @DisplayName("secretKey 키가 존재해야한다.")
     void t1() {assertThat(secretKeyPlain).isNotNull();}
 
+    // JWT는 HMAC 또는 RSA, ECDSA를 사용하는 공개키/개인키쌍을 서명
+    // 즉 토큰에 사용할 알고리즘은 HS256 외에도 HS512, RS256(RSASSA SHA-256), ES256(ECDSA P-256 curve SHA-256) 등이 있는데
+    // 그 중 HMAC을 선택하여 구현한 것
     @Test
     @DisplayName("sercretKey 원문으로 hmac 암호화 알고리즘에 맞는 SecretKey 객체를 만들 수 있다.")
     void t2() {
         String keyBase64Encoded = Base64.getEncoder().encodeToString(secretKeyPlain.getBytes());
         SecretKey secretKey = Keys.hmacShaKeyFor(keyBase64Encoded.getBytes());
+        assertThat(secretKey).isNotNull();
+    }
+
+    @Test
+    @DisplayName("JwtProvider 객체로 시크릿키 객체를 생성할 수 있다.")
+    void t3() {
+        SecretKey secretKey = jwtProvider.getSecretKey();
+
         assertThat(secretKey).isNotNull();
     }
 }
