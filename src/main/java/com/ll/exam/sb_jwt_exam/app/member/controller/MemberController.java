8ad00376728c54d5fe2fa58305991047ed4ba116
Ju Minji <alswl0w0@gmail.com>
2022-10-07 15:20:40 +0900

    [TEST] POST /member/login 테스트 통과
    * JWT 로그인 테스트통과 위해 스프링 시큐리티 설정 임의로 disable() 처리
    * Read json object from post http request
     -RestController, RequestBody 필요

---
+++ src/main/java/com/ll/exam/sb_jwt_exam/app/member/controller/MemberController.java
@@ -0,0 +1,24 @@
+package com.ll.exam.sb_jwt_exam.app.member.controller;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/member")
+public class MemberController {
+    @PostMapping("/login")
+    // JSON 객체 받는 클래스 따로 생성 필요
+    // Member 클래스의 username, password로 바로 받으면 제대로 안받아와짐
+    public String login(@RequestBody LoginDto loginDto) {
+        return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
+    }
+
+    @Data
+    public static class LoginDto {
+        private String username;
+        private String password;
+    }
+}
