e48572f7958212684eced8ce56d0488b6b2cf982
Ju Minji <alswl0w0@gmail.com>
2022-10-07 16:51:19 +0900

    [FEAT] 로그인처리 로직에 Authentication 응답헤더 추가

--- src/main/java/com/ll/exam/sb_jwt_exam/app/member/controller/MemberController.java
+++ src/main/java/com/ll/exam/sb_jwt_exam/app/member/controller/MemberController.java
@@ -6,13 +6,17 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
 @RequestMapping("/member")
 public class MemberController {
     @PostMapping("/login")
     // JSON 객체 받는 클래스 따로 생성 필요
     // Member 클래스의 username, password로 바로 받으면 제대로 안받아와짐
-    public String login(@RequestBody LoginDto loginDto) {
+    public String login(@RequestBody LoginDto loginDto, HttpServletResponse response) {
+        response.addHeader("Authentication", "JWT토큰");
+
         return "username : %s, password : %s".formatted(loginDto.getUsername(), loginDto.getPassword());
     }
 
