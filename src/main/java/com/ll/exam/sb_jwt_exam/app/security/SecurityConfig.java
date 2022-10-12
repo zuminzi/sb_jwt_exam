b07e8b7788bcc565c3a2e02fa9222bb4fe60fe83
Ju Minji <alswl0w0@gmail.com>
2022-10-12 13:22:55 +0900

    [CHORE] 로그인과 회원가입 엔드포인트 제외 그 외 기능들은 로그인 후에 사용할 수 있도록, JWT 커스텀필터 생성
    *JWT커스텀 필터에는 매 요청마다 토큰으로부터 사용자 인증정보 저장되도록(세션 역할 대체) 구현예정

--- src/main/java/com/ll/exam/sb_jwt_exam/app/security/SecurityConfig.java
+++ src/main/java/com/ll/exam/sb_jwt_exam/app/security/SecurityConfig.java
@@ -1,27 +1,41 @@
 package com.ll.exam.sb_jwt_exam.app.security;
 
+import com.ll.exam.sb_jwt_exam.app.security.filter.JwtAuthorizationFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import static org.springframework.security.config.http.SessionCreationPolicy.STATELESS;
 
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http, AuthenticationConfiguration authenticationConfiguration) throws Exception {
         http
+                .authorizeRequests( // /member/login과 /member/join 제외 모든 요청은 로그인 후 사용할 수 있도록
+                        authorizeRequests -> authorizeRequests
+                                .antMatchers("/member/login", "/member/join")
+                                .permitAll()
+                                .anyRequest()
+                                .authenticated() // 최소자격 : 로그인
+                )
                 .cors().disable() // 타 도메인에서 API 호출 가능
                 .csrf().disable() // CSRF 토큰 끄기
                 .httpBasic().disable() // httpBaic 로그인 방식 끄기
                 .formLogin().disable() // 폼 로그인 방식 끄기
                 .sessionManagement(sessionManagement ->
                         sessionManagement.sessionCreationPolicy(STATELESS)
-                ); // 세션 사용안함
+                ).addFilterBefore(
+                        jwtAuthorizationFilter, // JWT 관련 커스텀 필터 // 기존에 있던 UsernamePasswordAuthenticationFilter.class 전에 실행되도록 필터 추가
+                        UsernamePasswordAuthenticationFilter.class
+                );
 
         return http.build();
     }
---
+++ src/main/java/com/ll/exam/sb_jwt_exam/app/security/filter/JwtAuthorizationFilter.java
@@ -0,0 +1,20 @@
+package com.ll.exam.sb_jwt_exam.app.security.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Component
+public class JwtAuthorizationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        log.debug("JwtAuthorizationFilter 실행됨");
+    }
+}
