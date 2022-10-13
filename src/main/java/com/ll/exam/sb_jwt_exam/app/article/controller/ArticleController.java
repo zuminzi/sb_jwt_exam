8499b6e0cbb536871cae7a61bb74412f2bea9c23
Ju Minji <alswl0w0@gmail.com>
2022-10-13 15:18:03 +0900

    [FEAT] GET /article/1

--- src/main/java/com/ll/exam/sb_jwt_exam/app/article/controller/ArticleController.java
+++ src/main/java/com/ll/exam/sb_jwt_exam/app/article/controller/ArticleController.java
@@ -7,6 +7,7 @@ import com.ll.exam.sb_jwt_exam.util.Utility;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +27,28 @@ public class ArticleController {
                 RsData.successOf(
                         Utility.mapOf(
                                 "articles", articles
+                        )
+                )
+        );
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<RsData> detail(@PathVariable Long id) {
+        Article article = articleService.findById(id).orElse(null);
+
+        if (article == null) {
+            return Utility.spring.responseEntityOf(
+                    RsData.of(
+                            "F-1",
+                            "해당 게시물은 존재하지 않습니다."
+                    )
+            );
+        }
+
+        return Utility.spring.responseEntityOf(
+                RsData.successOf(
+                        Utility.mapOf(
+                                "article", article
                         )
                 )
         );
